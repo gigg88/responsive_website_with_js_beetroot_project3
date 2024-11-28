1c1225b1b453e4ff10ca1c1290fd14cba56e2adb
Christian Popp <popp.christian88@gmail.com>
2024-11-28 22:30:44 +0200

    final changes

--- react-project-3/src/pages/Header.js
+++ react-project-3/src/pages/Header.js
@@ -5,7 +5,7 @@ export default function Header () {
     return (
 
 
- <nav className="d-inline-flex mt-2 mt-md-0 mx-md-auto">
+ <nav className="d-inline-flex mt-2 pt-4 mx-md-auto">
   <Link to={'/'} className="me-3 py-2 link-body-emphasis text-decoration-none">Home</Link>
   <Link to={'CurrencyInfo'} className="me-3 py-2 link-body-emphasis text-decoration-none">Currencies</Link>
   <Link to={'Converter'} className="me-3 py-2 link-body-emphasis text-decoration-none">Converter</Link>
